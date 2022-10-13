1292ff7dfaf62268db42defa2f734a448d1ba04e
Bryan <bryanmonterom@gmail.com>
2022-10-12 20:10:20 -0400

    Add functionality to remove books

--- script.js
+++ script.js
@@ -11,15 +11,59 @@ function Book(title,author,pages, read){
     this.pages = pages;
     this.read = read;
     this.id=findId();
-    this.bookInfo = function(){
-        return (`The ${title} by ${author}, ${pages} pages ${read}`)
-    }
 }
+
+
+function buildBtnRemove(id){
+    var btn = document.createElement('input');
+    btn.type = "button";
+    btn.className = "btn-remove";
+    btn.value = "Remove";
+    btn.id=id;
+    btn.setAttribute('book-id',id);
+    btn.addEventListener('click',deleteBook)
+    return btn;
+}
+
+
+function deleteBook(e){
+    const target = e.target.id
+    myLibrary = myLibrary.filter((item)=> item.id != target);
+    tableBuilder(myLibrary);
+}
+function buildBtnStatus(){
+
+}
+
+function appendTableRow(newBook){
+    const tableBody = document.getElementById('tbody');
+    let tr = document.createElement('tr');
+        let tdTitle = document.createElement('td');
+        tdTitle.textContent = newBook.title;
+        let tdAuthor = document.createElement('td');
+        tdAuthor.textContent = newBook.author;
+        let tdPages = document.createElement('td');
+        tdPages.textContent = newBook.pages;
+        let tdRead = document.createElement('td');
+        tdRead.textContent = newBook.read;
+        const tdActions = document.createElement('td');
+        tdActions.appendChild(buildBtnRemove(newBook.id));
+        tr.appendChild(tdTitle);
+        tr.appendChild(tdAuthor);
+        tr.appendChild(tdPages);
+        tr.appendChild(tdRead);
+        tr.appendChild(tdActions);
+        tableBody.appendChild(tr);
+
+}
+
 
 
 function tableBuilder(){
     const tableBody = document.getElementById('tbody');
+    tableBody.innerHTML= "";
     myLibrary.forEach(element => {
+
         let tr = document.createElement('tr');
         let tdTitle = document.createElement('td');
         tdTitle.textContent = element.title;
@@ -28,29 +72,37 @@ function tableBuilder(){
         let tdPages = document.createElement('td');
         tdPages.textContent = element.pages;
         let tdRead = document.createElement('td');
-        tdRead.textContent = element.pages;
+        tdRead.textContent = element.read;
+        const tdActions = document.createElement('td');
+        tdActions.appendChild(buildBtnRemove(element.id));
         tr.appendChild(tdTitle);
         tr.appendChild(tdAuthor);
         tr.appendChild(tdPages);
         tr.appendChild(tdRead);
+        tr.appendChild(tdActions);
         tableBody.appendChild(tr);
 
     });
 }
 
-function fillBookInfo(){
+function fillBookInfoVars(){
 author = document.getElementById('author').value;
  title = document.getElementById('title').value;
  pages = document.getElementById('pages').value;
- read = document.getElementById('read').value;
+ read = document.getElementById('read').checked;
 }
 
 function addBookToLibrary(){
-    fillBookInfo();
-const newBook = new Book(title,author,pages,read)
-myLibrary.push(newBook);
-console.log(myLibrary);
-    tableBuilder();
+
+    fillBookInfoVars();
+    const newBook = new Book(title,author,pages,read)
+    myLibrary.push(newBook);
+    // console.log(myLibrary);
+    appendTableRow(newBook);
+}
+
+function remove(){
+
 }
 
 function findId(){
@@ -61,6 +113,7 @@ function findId(){
 function btnSaveEventListener(){
 }
 
+tableBuilder();
 findId();
 let btn = document.getElementById('save').addEventListener('click',addBookToLibrary);
 
